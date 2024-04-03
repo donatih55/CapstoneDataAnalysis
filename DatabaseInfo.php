601ed97677d75b5339731e2b420fb6e27f25fc68
donatih55 <donatihenrique@hotmail.com>
2024-04-02 22:36:49 -0500

    PHP won;t let me just use this file for variables, so am pushing the whole functions to make a DB conn with no passwords

--- DatabaseInfo.php
+++ DatabaseInfo.php
@@ -1,8 +1,22 @@
 <?php
 
-$servername = "";
-$username = "";
-$password = "";
-$dbname = "";
+
+function makeConn(){
+	//DB Credentials
+	$servername = "";
+    $username = "";
+    $password = "";
+    $dbname = "";
+
+	// Create connection
+	$conn = new mysqli($servername, $username, $password, $dbname);
+	// Check connectioncode, main, condition, group, order, limit
+	if (!$conn) {
+		echo "Bad Connection";
+		exit();
+	}
+	return $conn;
+}
+
 
 ?>
